c90f21ce1c6df7dd0766389192147e63d5098d46
jsuk10 <jsuk10@naver.com>
2021-01-05 02:40:31 +0900

    feat : add a log List

--- map/public/javascripts/map.js
+++ map/public/javascripts/map.js
@@ -1,7 +1,11 @@
 var infoContent = document.getElementById("infoContent");
+var log = document.getElementById("mapLog");
+var colorIndex = 0;
+const backGourndColorContainer = ["black", "white"];
+const textColorContainer = ["white", "black"];
 
 var mapOptions = {
-  center: new naver.maps.LatLng(36.016744, 127.817338),
+  center: new naver.maps.LatLng(36.416744, 127.817338),
   zoom: 8,
 };
 
@@ -9,9 +13,14 @@ var map = new naver.maps.Map("map", mapOptions);
 
 for (var i in data) {
   var target = data[i];
-  marker = new naver.maps.Marker({
+  Pointing(target.lat, target.lng);
+  AddList(target.content, target.time);
+}
+
+function Pointing(lat, lng) {
+  var marker = new naver.maps.Marker({
     map: map,
-    position: new naver.maps.LatLng(target.lat, target.lng),
+    position: new naver.maps.LatLng(lat, lng),
     icon: {
       content: "<div class='marker'></div>",
       url: "/images/pig.jpg",
@@ -22,20 +31,17 @@ for (var i in data) {
   });
 }
 
-function Pointing(lat, lng) {
-  console.log("d");
-  marker = new naver.maps.Marker({
-    map: map,
-    position: new naver.maps.LatLng(lat, lng),
 
-    icon: {
-      content: "<div class='marker'></div>",
-      url: "/images/pig.jpg",
-      size: new naver.maps.Size(50, 50),
-      origin: new naver.maps.Point(0, 0),
-      anchor: new naver.maps.Point(25, 25),
-    },
-  });
+function AddList(location, time) {
+  const span = document.createElement("span");
+  span.innerText = `${location}에서 \n ${time} 출몰`;
+  colorIndex > 0 ? 0 : 1;
+  span.style.background = backGourndColorContainer[colorIndex];
+  span.style.color = textColorContainer[colorIndex++];
+  log.appendChild(span);
+
+
+  // setTimeout(removeChild(span), 10000);
 }
 
 function setime() {
