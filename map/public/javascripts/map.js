a1be4b10c29f0f3454eca900f0e30d20264649f4
jsuk10 <jsuk10@naver.com>
2021-01-04 23:38:59 +0900

    feat : Pointing function & Read Data &Pointing

--- map/public/javascripts/map.js
+++ map/public/javascripts/map.js
@@ -1,16 +1,37 @@
-(() => {
-  var mapOptions = {
-    center: new naver.maps.LatLng(34.9506986, 127.4872429),
-    zoom: 11,
-  };
+var mapOptions = {
+  center: new naver.maps.LatLng(36.34128315236848, 127.36064560845736),
+  zoom: 8,
+};
 
-  var map = new naver.maps.Map("map", mapOptions);
+var map = new naver.maps.Map("map", mapOptions);
 
-  maker = new naver.maps.Marker({
+for (var i in data) {
+  var target = data[i];
+  marker = new naver.maps.Marker({
     map: map,
-    position: new naver.maps.LatLng(34.9506986, 127.4872429),
+    position: new naver.maps.LatLng(target.lat, target.lng),
     icon: {
-      content: "<div class='marker'>dd</div>",
+      content: "<div class='marker'></div>",
+      url: "/images/pig.jpg",
+      size: new naver.maps.Size(50, 50),
+      origin: new naver.maps.Point(0, 0),
+      anchor: new naver.maps.Point(25, 25),
     },
   });
-})();
+}
+
+function Pointing(lat, lng) {
+  console.log("d");
+  marker = new naver.maps.Marker({
+    map: map,
+    position: new naver.maps.LatLng(lat, lng),
+
+    icon: {
+      content: "<div class='marker'></div>",
+      url: "/images/pig.jpg",
+      size: new naver.maps.Size(50, 50),
+      origin: new naver.maps.Point(0, 0),
+      anchor: new naver.maps.Point(25, 25),
+    },
+  });
+}
