a42c883ba7d6c14fa735968b722b271575b213de
loin3 <ya6675@naver.com>
2021-01-05 18:58:50 +0900

    feature: add sse

--- map/routes/index.js
+++ map/routes/index.js
@@ -6,4 +6,51 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/update', function(req, res, next){
+  update(req, res, next);
+})
+
+router.get('/events', function(req, res, next){
+  eventsHandler(req, res, next);
+})
+
+
+let clients = [];
+let locations = [];
+function eventsHandler(req, res, next) {
+  const headers = {
+    'Content-Type': 'text/event-stream',
+    'Connection': 'keep-alive',
+    'Cache-Control': 'no-cache',
+    'Access-Control-Allow-Origin': '*'
+  };
+  res.writeHead(200, headers);
+
+  const data = `data: ${JSON.stringify(locations)}\n\n`;
+  res.write(data);
+  
+  const clientId = Date.now();
+  const newClient = {
+    id: clientId,
+    res
+  };
+  clients.push(newClient);
+  
+  req.on('close', () => {
+    console.log(`${clientId} Connection closed`);
+    clients = clients.filter(c => c.id !== clientId);
+  });
+}
+
+function sendEventsToAll(newLocation) {
+  clients.forEach(c => c.res.write(`data: ${JSON.stringify(newLocation)}\n\n`))
+}
+
+async function update(req, res, next) {
+  const newLocation = req.body;
+  locations.push(newLocation); 
+  res.json("ok");
+  return sendEventsToAll(newLocation);
+}
+
 module.exports = router;
